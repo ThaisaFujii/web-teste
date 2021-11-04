a213a231e3d0e9af233f81a3c06bc013dbfb2dde
thaisa-fujii <amanda_fujii@live.com>
2021-11-04 07:47:06 -0300

    first commit

---
+++ exemplo1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Exemplo 1</title>
+	</head>
+	<body>
+		<div class="container">
+		<?php 
+		echo 'Olá, hoje é dia '.date('d/m/Y'). '! ';
+		?>
+		</div>
+	</body>
+</html>
