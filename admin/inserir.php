8971c89a44780a54c5adf78d6a129e6f6180a346
thaisa-fujii <amanda_fujii@live.com>
2021-11-06 19:12:09 -0300

    commit table post + index structure

---
+++ admin/inserir.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS 
+    https://www.youtube.com/watch?v=OofP9BhzQMU 
+    -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Insert your post</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="text-center">
+            <a href="inserir.php"></a>
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,13 +5,29 @@
 		<title>Blog using PHP and MySQL</title>
 	</head>
 	<body>
-		
+		<header>
+        <?php 
+		echo 'Olá, hoje é dia '.date('d/m/Y'). '! '; ?>
+        <nav>
+        <div id="menu_Align">
+            <ul>
+                <li id="menu_Logo">logoTipo</li>
+                <li class="active"><a href="#">CATEGORIA 01</a></li>
+                <li><a href="#"></a>CATEGORIA 02</li>
+                <li><a href="#"></a>CATEGORIA 03</li>
+                <li><a href="#"></a>CATEGORIA 04</li>
+
+            </ul>
 		<div class="container">
-		<?php 
-		echo 'Olá, hoje é dia '.date('d/m/Y'). '! ';
-		
-		?>
-		
+            <h1>SEARCH</h1>
 		</div>
+        <div>
+            <h3>HOME > CATEGORIA 01</h3>
+        </div>
+        </nav>
+		
+		</header>
+        <section></section>
+		
 	</body>
 </html>
