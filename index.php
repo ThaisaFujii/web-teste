3f57f3c3d46cc6947dbfb6a5b6ef238a2b232afe
thaisa-fujii <amanda_fujii@live.com>
2021-11-06 17:09:34 -0300

    commit title correction and deleted exem file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Blog using PHP and MySQL</title>
+	</head>
+	<body>
+		
+		<div class="container">
+		<?php 
+		echo 'Olá, hoje é dia '.date('d/m/Y'). '! ';
+		
+		?>
+		
+		</div>
+	</body>
+</html>
